2f1a8d5a53e5d9facb635471f86106a3466071c5
huoyushi <huo_yushi@hotmail.com>
2017-10-14 12:22:57 +0800

    makefiles

---
+++ build/CMakeFiles/DAQ.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DAQ.dir/DAQ.cpp.o"
+  "libDAQ.pdb"
+  "libDAQ.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DAQ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
